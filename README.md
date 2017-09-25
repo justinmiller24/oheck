1f55ab3d67f84b99b15521aa8fafd3f3ab0fad13
Matthew Reynolds <matthew_reynolds@tsco.org>
2017-09-25 19:59:21 -0400

    Add eslint

--- README.md
+++ README.md
@@ -2,5 +2,11 @@
 Everybody's favorite card game! 
 
 Installation:
+
 1. npm install
 2. npm start
+
+Run Lint:
+
+1. npm run-script lint
+More at https://eslint.org/docs/about/
--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static(__dirname + '/public'));
 /*
  * ROUTES
  */
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/html/index.html');
 })
 
